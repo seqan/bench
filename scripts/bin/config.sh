e70bd86fd5f6bf2b2e9d011d5b839dba8ef4bffe
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-05-07 18:25:58 +0200

    Config

--- scripts/bin/config.sh
+++ scripts/bin/config.sh
@@ -1,10 +1,38 @@
 
-BIN=~/Code/seqan-builds/Release-Clang/bin
-DIR=~/Datasets/ibench
-ALPHABET=$1
-
-
 function vars_dna
+{
+    SRC=~/Datasets/ecoli
+
+    # text
+    TEXT_INPUT="genome.fasta"
+    TEXT_NAME=ecoli.txt
+    TEXT_COUNT=8
+    TEXT_SUM=32
+    TEXT_LENGTH=32
+
+    # pattern
+    PATTERN_INPUT="ecoli_1M.fastq"
+    PATTERN_NAME=ecoli.pat #.$PATTERN_LENGTHS[i]
+    PATTERN_COUNT=32
+    PATTERN_SUM=32
+    PATTERN_LENGTH=8
+    PATTERN_LENGTHS="5 10 15 20"
+#    PATTERN_COUNTS="10000 100000 1000000"
+
+    # index
+    INDEX_NAME=ecoli #.$INDEX_TYPE
+    INDEX_TYPE="sa esa fm-tl fm-wt"
+
+    # visit
+    VISIT_DEPTH=$(seq 1 30)
+
+    # query
+#    QUERY_LOCATE=15
+    QUERY_ERRORS="0 1"
+    QUERY_ALGORITHM="single dfs"
+}
+
+function vars_dna_XXX
 {
     SRC=~/Datasets/celegans
 
@@ -42,7 +70,7 @@ function vars_protein
     SRC=~/Datasets/uniprot
 
     # text
-    TEXT_INPUT="uniprot_sprot.fasta"
+    TEXT_INPUT="uniprot_sprot.fasta.gz"
     TEXT_NAME=sprot.txt
     TEXT_COUNT=32
     TEXT_SUM=32
@@ -100,17 +128,24 @@ function cmd_query
 }
 
 
+BIN=~/Code/seqan-builds/Release-Clang/bin
+DIR=~/Datasets/ibench
+ALPHABET=$1
+
 vars_$ALPHABET
+
 
 # prepare text
 cmd_prepare $SRC/$TEXT_INPUT $DIR/$TEXT_NAME $ALPHABET $TEXT_COUNT $TEXT_SUM $TEXT_LENGTH
 echo $CMD
+$CMD
 
 # construct text index
 for index_type in $INDEX_TYPE;
 do
     cmd_construct $DIR/$TEXT_NAME $DIR/$INDEX_NAME $ALPHABET $TEXT_COUNT $TEXT_SUM $TEXT_LENGTH $index_type
     echo $CMD
+    $CMD
 done
 
 # visit text index
@@ -120,22 +155,28 @@ do
     do
         cmd_visit $depth $DIR/$INDEX_NAME $ALPHABET $TEXT_COUNT $TEXT_SUM $TEXT_LENGTH $index_type
         echo $CMD
+        $CMD
     done
 done
 
 # prepare patterns
 for pattern_length in $PATTERN_LENGTHS;
 do
-    cmd_prepare $SRC/$PATTERN_INPUT $DIR/$PATTERN_NAME.$pattern_length $ALPHABET $PATTERN_COUNT $PATTERN_SUM $pattern_length
+    cmd_prepare $SRC/$PATTERN_INPUT $DIR/$PATTERN_NAME.$pattern_length $ALPHABET $PATTERN_COUNT $PATTERN_SUM $PATTERN_LENGTH $pattern_length
     echo $CMD
+    $CMD
 done
 
 # query patterns
 for index_type in $INDEX_TYPE;
 do
-    for pattern_length in $PATTERN_LENGTHS;
+    for errors in $QUERY_ERRORS;
     do
-        cmd_query $DIR/$INDEX_NAME $DIR/$PATTERN_NAME $ALPHABET $TEXT_COUNT $TEXT_SUM $TEXT_LENGTH $index_type $PATTERN_LENGTH $pattern_length 0 single
-        echo $CMD
+        for pattern_length in $PATTERN_LENGTHS;
+        do
+            cmd_query $DIR/$INDEX_NAME $DIR/$PATTERN_NAME $ALPHABET $TEXT_COUNT $TEXT_SUM $TEXT_LENGTH $index_type $pattern_length $errors single
+            echo $CMD
+            $CMD
+        done
     done
 done

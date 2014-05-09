49ee0bdcb5b7b1d613dc57a7d480baf76d6fc191
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-05-09 18:08:14 +0200

    Dna

--- scripts/tex/tex.R
+++ scripts/tex/tex.R
@@ -4,11 +4,13 @@ library("ggplot2")
 library("scales")
 
 PATH="/Users/esiragusa/Datasets/ibench"
+ALPHABET='dna'
+ALPHSIZE=4
 
 FILENAME_VISIT=paste(paste(PATH, "visit", sep='/'), "tsv", sep='.')
 FILENAME_QUERY=paste(paste(PATH, "query", sep='/'), "tsv", sep='.')
-PLOT_VISIT=paste(paste(PATH, "visit", sep='/'), "pdf", sep='.')
-PLOT_QUERY=paste(paste(PATH, "query", sep='/'), "pdf", sep='.')
+PLOT_VISIT=paste(paste(PATH, "visit", sep='/'), ALPHABET, "pdf", sep='.')
+PLOT_QUERY=paste(paste(PATH, "query", sep='/'), ALPHABET, "pdf", sep='.')
 
 options(scipen=999)
 
@@ -53,14 +55,14 @@ if ((R = load_file(FILENAME_VISIT))$ok)
 } else
   print(paste("NOT FOUND:", FILENAME_VISIT))
 
-table_visit = subset(TABLE_VISIT, alphabet=='dna', select=c(index, depth, time))
-table_nodes = subset(TABLE_VISIT, alphabet=='dna' & index=='fm-tl', select=c(depth, nodes))
-table_nodes[,'nodes'] = log(table_nodes[, 'nodes'], 4)
+table_visit = subset(TABLE_VISIT, alphabet==ALPHABET, select=c(index, depth, time))
+table_nodes = subset(TABLE_VISIT, alphabet==ALPHABET & index=='fm-tl', select=c(depth, nodes))
+table_nodes[,'nodes'] = log(table_nodes[, 'nodes'], ALPHSIZE)
 
 ggplot() +
   geom_line(data=table_visit, aes(x=depth, y=log10(time), group=index, shape=index, color=index)) +
   geom_point(data=table_visit, aes(x=depth, y=log10(time), group=index, shape=index, color=index), size=3) +
-  geom_line(data=table_nodes, aes(x=depth, y=nodes/4), linetype='dotted') +
+  geom_line(data=table_nodes, aes(x=depth, y=nodes/ALPHSIZE), linetype='dotted') +
   xlab("depth") +
   ylab("log10 seconds") +
   theme_bw(base_size=12, base_family="Helvetica")
@@ -76,8 +78,8 @@ if ((R = load_file(FILENAME_QUERY))$ok)
 } else
   print(paste("NOT FOUND:", FILENAME_QUERY))
 
-table_query_0 = subset(TABLE_QUERY, alphabet=='dna' & errors==0, select=c(index, plength, time))
-table_query_1 = subset(TABLE_QUERY, alphabet=='dna' & errors==1, select=c(index, plength, time))
+table_query_0 = subset(TABLE_QUERY, alphabet==ALPHABET & errors==0, select=c(index, plength, time))
+table_query_1 = subset(TABLE_QUERY, alphabet==ALPHABET & errors==1, select=c(index, plength, time))
 
 ggplot() +
   geom_line(data=table_query_0, aes(x=plength, y=time, group=index, shape=index, color=index)) +
@@ -90,17 +92,3 @@ ggplot() +
   theme_bw(base_size=12, base_family="Helvetica")
 
 ggsave(file=PLOT_QUERY)
-
-
-
-# table_occ = subset(TABLE_QUERY, alphabet=='dna' & index=='fm-tl', select=c(plength, errors, occurrences))
-# table_occ[,'occurrences'] = table_occ[, 'occurrences'] / 1000000
-# 
-# ggplot() +
-#   geom_line(data=table_occ, aes(x=plength, y=occurrences, group=errors, color=errors)) +
-#   xlab("pattern length") +
-#   ylab("occurrences") +
-#   scale_y_log10() +
-#   ggtitle("Occurrenecs by pattern length") +
-#   theme_bw(base_size=12, base_family="Helvetica")
-

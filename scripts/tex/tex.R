9057dd7831b264259b662baf60145e5faf04f094
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-05-09 16:28:30 +0200

    Tex

--- scripts/tex/tex.R
+++ scripts/tex/tex.R
@@ -3,9 +3,11 @@ library("xtable")
 library("ggplot2")
 
 PATH="/Users/esiragusa/Datasets/ibench"
-FILENAME_CONSTRUCT=paste(paste(PATH, "construct", sep='/'), "tsv", sep='.')
+
 FILENAME_VISIT=paste(paste(PATH, "visit", sep='/'), "tsv", sep='.')
 FILENAME_QUERY=paste(paste(PATH, "query", sep='/'), "tsv", sep='.')
+PLOT_VISIT=paste(paste(PATH, "visit", sep='/'), "pdf", sep='.')
+PLOT_QUERY=paste(paste(PATH, "query", sep='/'), "pdf", sep='.')
 
 options(scipen=999)
 
@@ -56,10 +58,13 @@ table_visit = subset(TABLE_VISIT, alphabet=='dna', select=c(index, depth, time))
 ggplot() +
   geom_line(data=table_visit, aes(x=depth, y=time, group=index, shape=index, color=index)) +
   geom_point(data=table_visit, aes(x=depth, y=time, group=index, shape=index, color=index), size=3) +
-  xlab("visit depth") +
-  ylab("visit time") +
+  xlab("depth") +
+  ylab("log10 seconds") +
   scale_y_log10() +
+  ggtitle("Bounded DFS time by depth") +
   theme_bw(base_size=12, base_family="Helvetica")
+
+ggsave(file=PLOT_VISIT)
 
 
 ### PLOT QUERY ###
@@ -70,14 +75,21 @@ if ((R = load_file(FILENAME_QUERY))$ok)
 } else
   print(paste("NOT FOUND:", FILENAME_QUERY))
 
-table_query = subset(TABLE_QUERY, alphabet=='dna' & errors==0, select=c(index, plength, time))
-#fillup = data.frame(depth=c(1:10), rate=log(c(1:10)))
+table_query_0 = subset(TABLE_QUERY, alphabet=='dna' & errors==0, select=c(index, plength, time))
+table_query_1 = subset(TABLE_QUERY, alphabet=='dna' & errors==1, select=c(index, plength, time))
 
 ggplot() +
-  geom_line(data=table_query, aes(x=plength, y=time, group=index, shape=index, color=index)) +
-  geom_point(data=table_query, aes(x=plength, y=time, group=index, shape=index, color=index), size=3) +
+  geom_line(data=table_query_0, aes(x=plength, y=time, group=index, shape=index, color=index)) +
+  geom_point(data=table_query_0, aes(x=plength, y=time, group=index, shape=index, color=index), size=3) +
+  geom_line(data=table_query_1, aes(x=plength, y=time, group=index, shape=index, color=index), linetype='dashed') +  
+  geom_point(data=table_query_1, aes(x=plength, y=time, group=index, shape=index, color=index), size=3) +
   xlab("pattern length") +
-  ylab("average time") +
+  ylab("log10 seconds") +
+  scale_y_log10() +
+  ggtitle("Search time by pattern length") +
   theme_bw(base_size=12, base_family="Helvetica")
+
+#fillup = data.frame(depth=c(1:10), rate=log(c(1:10)))
 #+ geom_line(data=fillup, aes(x=depth, y=rate), linetype='dashed')
 
+ggsave(file=PLOT_QUERY)

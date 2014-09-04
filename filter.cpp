f6f7336b8dc67f491bb61d94244c5a681b5fd0b4
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-09-04 13:08:24 +0200

    Qgrams

--- filter.cpp
+++ filter.cpp
@@ -61,15 +61,15 @@ using namespace seqan;
 // ----------------------------------------------------------------------------
 // Class Stats
 // ----------------------------------------------------------------------------
-
-struct Stats
-{
-    double          filterTime;
-
-    Stats() :
-        filterTime()
-    {}
-};
+//
+//struct Stats
+//{
+//    double          filterTime;
+//
+//    Stats() :
+//        filterTime()
+//    {}
+//};
 
 // ----------------------------------------------------------------------------
 // Class Options
@@ -82,7 +82,7 @@ struct Options : BaseOptions
 
     enum AlgorithmType
     {
-        ALGO_SEEDS_EXACT, ALGO_SEEDS_APPROXIMATE, ALGO_QGRAMS_CONTIGUOUS, ALGO_QGRAMS_GAPPED
+        ALGO_SEEDS, ALGO_QGRAMS
     };
 
     CharString      queryFile;
@@ -91,20 +91,30 @@ struct Options : BaseOptions
     TList           algorithmTypeList;
 
     unsigned        errorRate;
-    bool            edit;
+    bool            editDistance;
     bool            verify;
+
+    unsigned        seedsErrors;
+    bool            seedsOnline;
+
+    unsigned        qgramsWeight;
+    unsigned        qgramsThreshold;
+    std::string     qgramsShape;
 
     Options() :
         BaseOptions(),
-        algorithmType(ALGO_SEEDS_EXACT),
+        algorithmType(ALGO_SEEDS),
         errorRate(0),
-        edit(false),
-        verify(false)
+        editDistance(false),
+        verify(false),
+        seedsErrors(0),
+        seedsOnline(true),
+        qgramsWeight(10),
+        qgramsThreshold(1),
+        qgramsShape()
     {
-        algorithmTypeList.push_back("seeds-exact");
-        algorithmTypeList.push_back("seeds-approximate");
-        algorithmTypeList.push_back("qgrams-contiguous");
-        algorithmTypeList.push_back("qgrams-gapped");
+        algorithmTypeList.push_back("seeds");
+        algorithmTypeList.push_back("qgrams");
     }
 };
 
@@ -123,7 +133,7 @@ inline void setupArgumentParser(ArgumentParser & parser, TOptions const & option
     setShortDescription(parser, "Benchmark filtration efficiency");
     setCategory(parser, "Stringology");
 
-    addUsageLine(parser, "[\\fIOPTIONS\\fP] <\\fITEXT FILE\\fP> <\\fIQUERY FILE\\fP>");
+    addUsageLine(parser, "[\\fIOPTIONS\\fP] <\\fITEXT\\fP> <\\fIQUERY FILE\\fP>");
 
     addArgument(parser, ArgParseArgument(ArgParseArgument::INPUTFILE));
     addArgument(parser, ArgParseArgument(ArgParseArgument::INPUTFILE));
@@ -143,10 +153,46 @@ inline void setupArgumentParser(ArgumentParser & parser, TOptions const & option
     addOption(parser, ArgParseOption("ed", "edit-distance", "Edit distance. Default: Hamming distance."));
     addOption(parser, ArgParseOption("vf", "verify", "Verify candidate locations. Default: filter only."));
 
-    addSection(parser, "Seeds Filtering Options");
+    addSection(parser, "Seed Filtering Options");
+    addOption(parser, ArgParseOption("se", "seeds-errors", "Set maximum seed errors.", ArgParseOption::INTEGER));
+    setMinValue(parser, "seeds-errors", "0");
+    setMaxValue(parser, "seeds-errors", "3");
+    setDefaultValue(parser, "seeds-errors", options.seedsErrors);
+//    addOption(parser, ArgParseOption("si", "seeds-index", "Text index file.", ArgParseOption::INPUTFILE));
+    addOption(parser, ArgParseOption("so", "seeds-online", "Online filter - for exact seeds only. Default: indexed.", ArgParseOption::INTEGER));
 
     addSection(parser, "q-Gram Filtering Options");
+    addOption(parser, ArgParseOption("qw", "qgrams-weight", "Set q-gram weight.", ArgParseOption::INTEGER));
+    setMinValue(parser, "qgrams-weight", "4");
+    setMaxValue(parser, "qgrams-weight", "31");
+    setDefaultValue(parser, "qgrams-weight", options.qgramsWeight);
+    addOption(parser, ArgParseOption("qt", "qgrams-threshold", "Set minimum q-gram threshold.", ArgParseOption::INTEGER));
+    setMinValue(parser, "qgrams-threshold", "1");
+    setMaxValue(parser, "qgrams-threshold", "100");
+    setDefaultValue(parser, "qgrams-threshold", options.qgramsThreshold);
+    addOption(parser, ArgParseOption("qs", "qgrams-shape", "Set q-gram shape.", ArgParseOption::STRING, "BITSTRING"));
+}
 
+// ----------------------------------------------------------------------------
+// Function parseShape()
+// ----------------------------------------------------------------------------
+
+template <typename TString>
+inline unsigned shapeWeight(TString const & shape)
+{
+    unsigned ones = count(shape, '1');
+    unsigned zeros = count(shape, '0');
+
+    if (ones + zeros != length(shape))
+        throw RuntimeError("Shape must be a binary string");
+
+    if (ones < 4 || ones > 31)
+        throw RuntimeError("Shape must contain at least 4 and at most 31 '1's");
+
+    if (front(shape) != '1' || back(shape) != '1')
+        throw RuntimeError("Shape must start and end with '1'");
+
+    return ones;
 }
 
 // ----------------------------------------------------------------------------
@@ -170,10 +216,26 @@ inline parseCommandLine(TOptions & options, ArgumentParser & parser, int argc, c
     getAlphabetType(options, parser);
     getIndexType(options, parser);
     getTextLimits(options, parser);
+
     getAlgorithmType(options, parser);
-    getOptionValue(options.edit, parser, "edit-distance");
+    getOptionValue(options.editDistance, parser, "edit-distance");
     getOptionValue(options.errorRate, parser, "error-rate");
     getOptionValue(options.verify, parser, "verify");
+
+    getOptionValue(options.seedsErrors, parser, "seeds-errors");
+    getOptionValue(options.seedsOnline, parser, "seeds-online");
+
+    if (isSet(parser, "qgrams-shape"))
+    {
+        getOptionValue(options.qgramsShape, parser, "qgrams-shape");
+        options.qgramsWeight = shapeWeight(options.qgramsShape);
+    }
+    else
+    {
+        getOptionValue(options.qgramsWeight, parser, "qgrams-weight");
+        options.qgramsShape.assign(options.qgramsWeight, '1');
+    }
+    getOptionValue(options.qgramsThreshold, parser, "qgrams-threshold");
 
     return ArgumentParser::PARSE_OK;
 }
@@ -227,33 +289,32 @@ inline parseCommandLine(TOptions & options, ArgumentParser & parser, int argc, c
 //}
 
 // ----------------------------------------------------------------------------
-// Function filter()
+// Function filterOnline()
 // ----------------------------------------------------------------------------
 // q-Gram filters.
 
-template <typename TText, typename TQueries>
-inline unsigned long filter(Options const & options, Stats & stats, TText & text, TQueries & queries, Nothing)
+template <typename TText, typename TQueries, typename TAlgorithm, typename TShape>
+inline unsigned long filterOnline(Options const & options, TText & text, TQueries & queries, TAlgorithm const &, TShape const & shape)
 {
-    typedef Shape<Dna, UngappedShape<11> >                  TShape;
 //    typedef IndexQGram<TShape, OpenAddressing>              TIndexSpec;
     typedef IndexQGram<TShape>                              TIndexSpec;
     typedef Index<TQueries, TIndexSpec>                     TPatternIndex;
-    typedef Pattern<TPatternIndex, Swift<SwiftSemiGlobal> > TPattern;
+    typedef Pattern<TPatternIndex, TAlgorithm>              TPattern;
 
     typedef typename Value<TText>::Type const               THaystack;
     typedef typename Size<TText>::Type                      THaystackSize;
-    typedef Finder<THaystack, Swift<SwiftSemiGlobal> >      TFinder;
+    typedef Finder<THaystack, TAlgorithm>                   TFinder;
 
     unsigned long count = 0;
 
-    double timer = sysTime();
+//    double timer = sysTime();
 
-    TPatternIndex patternIndex(queries);
+    TPatternIndex patternIndex(queries, shape);
     TPattern pattern(patternIndex);
 
-    unsigned threshold = 1;
-    for (unsigned i = 0; i < length(queries); ++i)
-        setMinThreshold(pattern, i, threshold);
+    pattern.params.minThreshold = options.qgramsThreshold;
+//    setStepSize(patternIndex, 0);
+//    _patternInit(pattern, options.errorRate / 100.0, 0);
 
     for (THaystackSize i = 0; i < length(text); ++i)
     {
@@ -261,7 +322,7 @@ inline unsigned long filter(Options const & options, Stats & stats, TText & text
         while (find(finder, pattern, (double)(length(front(queries)) * options.errorRate)))
         {
             count++;
-            // Verify match.
+
 //            Finder<TContigSeq> verifyFinder(fragStore.contigStore[i].seq);
 //            setPosition(verifyFinder, beginPosition(finder));
 //            Pattern<TReadSeq, HammingSimple> verifyPattern(fragStore.readSeqStore[position(pattern).i1]);
@@ -274,9 +335,30 @@ inline unsigned long filter(Options const & options, Stats & stats, TText & text
 //            }
         }
     }
-    stats.filterTime = sysTime() - timer;
+
+//    stats.filterTime = sysTime() - timer;
 
     return count;
+}
+
+// ----------------------------------------------------------------------------
+// Function filterOnline()
+// ----------------------------------------------------------------------------
+
+template <typename TText, typename TQueries, typename TAlgorithm>
+inline unsigned long filterOnline(Options const & options, TText & text, TQueries & queries, TAlgorithm const & algo)
+{
+//    Shape<Dna, UngappedShape<9> > contiguous_w9;
+    Shape<Dna, SimpleShape>     contiguous;
+    Shape<Dna, GenericShape>    gapped;
+
+    if (stringToShape(contiguous, options.qgramsShape))
+        return filterOnline(options, text, queries, algo, contiguous);
+
+    if (stringToShape(gapped, options.qgramsShape))
+        return filterOnline(options, text, queries, algo, gapped);
+
+    throw RuntimeError("Unsupported q-gram shape");
 }
 
 // ----------------------------------------------------------------------------
@@ -285,13 +367,10 @@ inline unsigned long filter(Options const & options, Stats & stats, TText & text
 // Dispatch locate, distance, and search algorithm.
 
 template <typename TText, typename TQueries>
-inline unsigned long filter(Options const & options, Stats & stats, TText & text, TQueries & queries)
+inline unsigned long filter(Options const & options, TText & text, TQueries & queries)
 {
     switch (options.algorithmType)
     {
-//    case Options::ALGO_SEEDS_EXACT:
-//        return ...;
-
 //    case Options::ALGO_SEEDS_APPROXIMATE:
 //        switch (options.seedsErrors)
 //        {
@@ -301,11 +380,12 @@ inline unsigned long filter(Options const & options, Stats & stats, TText & text
 //            return filter(options, stats, index, queries, TLocate(), Backtracking<HammingDistance, Threshold<2> >(), BfsIterator());
 //        }
 
-    case Options::ALGO_QGRAMS_CONTIGUOUS:
-        return filter(options, stats, text, queries, Nothing());
+//    case Options::ALGO_SEEDS:
+//        return filterOffline(options, text, queries);
+//        return filterOnline(options, stats, text, queries, Pigeonhole<void>());
 
-//    case Options::ALGO_QGRAMS_GAPPED:
-//        return ...;
+    case Options::ALGO_QGRAMS:
+        return filterOnline(options, text, queries, Swift<SwiftSemiGlobal>());
 
     default:
         throw RuntimeError("Unsupported filter");
@@ -338,21 +418,21 @@ inline void run(Options const & options)
     if (!open(queries, toCString(options.queryFile)))
         throw RuntimeError("Error while loading queries");
 
-    Stats stats;
+//    Stats stats;
 
-    unsigned long hitsCount = filter(options, stats, text, queries);
+    unsigned long hitsCount = filter(options, text, queries);
 
-    if (options.tsv)
-    {
+//    if (options.tsv)
+//    {
 //        std::cout << hitsCount << '\t' << std::fixed << stats.countTime << '\t' << stats.preprocessingTime << std::endl;
-    }
-    else
-    {
+//    }
+//    else
+//    {
         std::cout << length(queries) << " queries" << std::endl;
         std::cout << lengthSum(queries) << " symbols" << std::endl;
         std::cout << hitsCount << " hits" << std::endl;
 //        std::cout << std::fixed << stats.countTime << " + " << stats.preprocessingTime << " sec" << std::endl;
-    }
+//    }
 }
 
 int main(int argc, char const ** argv)

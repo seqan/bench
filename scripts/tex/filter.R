0870951361341155bbcbefb63f2e098bf41de300
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-09-09 18:22:51 +0200

    R filter plots

--- scripts/tex/filter.R
+++ scripts/tex/filter.R
@@ -2,7 +2,7 @@ library("reshape")
 library("xtable")
 library("ggplot2")
 library("scales")
-#library("Cairo")
+library("Cairo")
 
 INPUT="/Users/esiragusa/Code/seqan/core/apps/ibench/scripts/resources"
 OUTPUT="/Users/esiragusa/Documents/Dissertation/plots"
@@ -12,6 +12,13 @@ FONT_SIZE=10
 POINT_SIZE=2
 FONT_FAMILY='Cambria'
 
+FILTER_NAMES=c("exact","approximate") #,"contiguous","gapped")
+FILTER_LABELS=c("Exact seeds","Approximate seeds") #,"Contiguous q-grams", "Gapped q-grams")
+
+DATASET='celegans'
+ALPHABET='dna'
+ALPHSIZE=4
+PLENGTHS=c(30)
 
 ### FUNCTIONS ###
 
@@ -55,19 +62,110 @@ scientific_10 <- function(x)
 }
 
 
-### PLOT FILTER ###
+### PLOT ONLINE VS OFFLINE RUNTIME ###
 
-FILTER_NAMES=c("seeds.exact","seeds.approximate") #,"qgrams.contiguous","qgrams.gapped")
-FILTER_LABELS=c("Exact seeds","Approximate seeds") #,"Contiguous q-grams", "Gapped q-grams")
+PLENGTH=100
+ERRORS=4
 
-TABLE_FILTERS <- data.frame()
+FILENAME_ONOFF=paste(paste(INPUT, 'onoff', sep='/'), "tsv", sep='.')
+PLOT_ONOFF=paste(paste(OUTPUT, "onoff", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
+
+if ((R = load_file(FILENAME_ONOFF))$ok)
+{
+  TABLE_ONOFF <- R$tsv;
+} else
+  print(paste("NOT FOUND:", FILENAME_ONOFF))
+
+table_onoff = subset(TABLE_ONOFF, alphabet==ALPHABET & dataset==DATASET & plength==PLENGTH & errors==ERRORS, select=c(filter, pcount, time))
+table_onoff <- transform(table_onoff, time = time / pcount)
+
+ggplot() +
+  geom_line(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), linetype='solid') +
+  geom_point(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
+  scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+  scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+  xlab("Patterns") +
+  ylab("Time (seconds/patterns)") +
+  scale_x_log10() +
+  theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
+
+ggsave(file=PLOT_ONOFF, scale=SCALE, device=cairo_pdf) 
+
+
+
+### PLOT MATCHES ###
+
+FILENAME_MATCHES=paste(paste(INPUT, 'matches', sep='/'), "tsv", sep='.')
+
+if ((R = load_file(FILENAME_MATCHES))$ok)
+{
+  TABLE_MATCHES <- R$tsv;
+} else
+  print(paste("NOT FOUND:", FILENAME_MATCHES))
+
+
+### PLOT PPV ###
+
+FILENAME_FILTER=paste(paste(INPUT, 'filter', sep='/'), "tsv", sep='.')
+PLOT_PPV=paste(paste(OUTPUT, "ppv", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
+
+if ((R = load_file(FILENAME_FILTER))$ok)
+{
+  TABLE_FILTERS <- R$tsv;
+} else
+  print(paste("NOT FOUND:", FILENAME_FILTER))
+
+TABLE_FILTERS = merge(TABLE_FILTERS, TABLE_MATCHES, by=c('alphabet', 'dataset', 'pcount', 'plength', 'errors', 'distance'), all=TRUE)
+
+table_ppv = subset(TABLE_FILTERS, alphabet==ALPHABET & dataset==DATASET & plength==PLENGTH, select=c(filter, errors, distance, verifications, matches))
+table_ppv$ppv <- table_ppv$matches / table_ppv$verification
+
+ggplot() +
+  geom_line(data=table_ppv, aes(x=errors, y=ppv, group=filter, shape=filter, color=filter), linetype='solid') +
+  geom_point(data=table_ppv, aes(x=errors, y=ppv, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
+  scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+  scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+  xlab("Errors") +
+  ylab("PPV (matches/verifications)") +
+  theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
+
+ggsave(file=PLOT_MULTI_IDX, scale=SCALE, device=cairo_pdf) 
+
+
+### PLOT RUNTIME ###
+
+for (DISTANCE in c('hamming','edit'))
+{
+  table_times = subset(TABLE_FILTERS, alphabet==ALPHABET & dataset==DATASET & distance==DISTANCE & plength==PLENGTH, select=c(filter, errors, ftime, time))
+  
+  PLOT_MULTI_IDX=paste(paste(OUTPUT, "fspeed", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
+  
+  ggplot() +
+    geom_line(data=table_times, aes(x=errors, y=time, group=filter, shape=filter, color=filter), linetype='solid') +
+    geom_point(data=table_times, aes(x=errors, y=time, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
+    geom_line(data=table_times, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), linetype='dashed') +
+    geom_point(data=table_times, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), size=POINT_SIZE-1) +
+    scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+    scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+    xlab("Errors") +
+    ylab("Time (seconds)") +
+    scale_y_log10() +
+    theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
+  
+  ggsave(file=PLOT_MULTI_IDX, scale=SCALE, device=cairo_pdf) 
+}
+
+
+### PLOT VERIFICATIONS ###
+
+TABLE_VERIFICATIONS <- data.frame()
 for (FILTER in FILTER_NAMES)
 {
   FILENAME_FILTER=paste(paste(INPUT, FILTER, sep='/'), "tsv", sep='.')
   
   if ((R = load_file(FILENAME_FILTER))$ok)
   {
-    TABLE_FILTERS <- R$tsv;
+    TABLE_VERIFICATIONS <- R$tsv;
   } else
     print(paste("NOT FOUND:", FILENAME_FILTER))
 }

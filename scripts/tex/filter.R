e47b2a768b4001ac7db5140773655d0c03e92d60
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-09-21 21:46:04 +0200

    R

--- scripts/tex/filter.R
+++ scripts/tex/filter.R
@@ -12,13 +12,14 @@ FONT_SIZE=10
 POINT_SIZE=2
 FONT_FAMILY='Cambria'
 
-FILTER_NAMES=c("seeds_0","seeds_1","seeds_2","qgrams_0","qgrams_1") # qgrams_gapped
+FILTER_NAMES=c("seeds_0","seeds_1","seeds_2","qgrams_0","qgrams_1","qgrams_2","qgrams_gapped")
 FILTER_LABELS=c("Exact seeds",
-                "1-approximate seeds",
-                "2-approximate seeds",
-                "Contiguous q-grams (t >= 1)",
-                "Contiguous q-grams (t >= 2)")
-#"Gapped q-grams"
+                "1-Apx seeds",
+                "2-Apx seeds",
+                "Contig q-grams (t >= 1)",
+                "Contig q-grams (t >= 2)",
+                "Contig q-grams (t >= 4)",
+                "Gapped q-grams")
 
 DATASET='celegans'
 ALPHABET='dna'
@@ -66,36 +67,6 @@ scientific_10 <- function(x)
 {
   parse(text=gsub("e", " %*% 10^", scientific_format()(x)))
 }
-
-
-### PLOT ONLINE VS OFFLINE RUNTIME ###
-
-# ERRORS=4
-# 
-# FILENAME_ONOFF=paste(paste(INPUT, 'onoff', sep='/'), "tsv", sep='.')
-# PLOT_ONOFF=paste(paste(OUTPUT, "onoff", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
-# 
-# if ((R = load_file(FILENAME_ONOFF))$ok)
-# {
-#   TABLE_ONOFF <- R$tsv;
-# } else
-#   print(paste("NOT FOUND:", FILENAME_ONOFF))
-# 
-# table_onoff = subset(TABLE_ONOFF, alphabet==ALPHABET & dataset==DATASET & plength==PLENGTH & errors==ERRORS, select=c(filter, pcount, time))
-# table_onoff <- transform(table_onoff, time = time / pcount)
-# 
-# ggplot() +
-#   geom_line(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), linetype='solid') +
-#   geom_point(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
-#   scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
-#   scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
-#   xlab("Patterns") +
-#   ylab("Time (seconds/patterns)") +
-#   scale_x_log10() +
-#   theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
-# 
-# ggsave(file=PLOT_ONOFF, scale=SCALE, device=cairo_pdf) 
-
 
 
 ### PLOT PPV ###
@@ -153,24 +124,54 @@ TABLE_RUNTIME = rename(TABLE_RUNTIME, c("time.x"="ftime", "time.y"="time"))
 
 for (DISTANCE in DISTANCES)
 {
-  table_runtime = subset(TABLE_RUNTIME, alphabet==ALPHABET & dataset==DATASET & distance==DISTANCE & plength==PLENGTH, select=c(filter, errors, time))
+  table_runtime = subset(TABLE_RUNTIME, alphabet==ALPHABET & dataset==DATASET & distance==DISTANCE & plength==PLENGTH, select=c(filter, errors, pcount, time, ftime))
+  table_runtime <- transform(table_runtime, time = time / pcount, ftime = ftime / pcount)
   
   PLOT_RUNTIME=paste(paste(OUTPUT, "filter_runtime", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
   
   ggplot() +
     geom_line(data=table_runtime, aes(x=errors, y=time, group=filter, shape=filter, color=filter), linetype='solid') +
     geom_point(data=table_runtime, aes(x=errors, y=time, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
-#    geom_line(data=table_runtime, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), linetype='dashed') +
-#    geom_point(data=table_runtime, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), size=POINT_SIZE-1) +
+    geom_line(data=table_runtime, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), linetype='dashed') +
+    geom_point(data=table_runtime, aes(x=errors, y=ftime, group=filter, shape=filter, color=filter), size=POINT_SIZE-1) +
     scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
     scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
     xlab("Errors") +
-    ylab("Time (seconds)") +
-    scale_y_log10() +
+    ylab("Time (seconds/pattern)") +
+    scale_y_log10(labels=scientific_10) +
     theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
   
   ggsave(file=PLOT_RUNTIME, scale=SCALE, device=cairo_pdf) 
 }
+
+
+### PLOT ONLINE VS OFFLINE RUNTIME ###
+
+# ERRORS=4
+# 
+# FILENAME_ONOFF=paste(paste(INPUT, 'onoff', sep='/'), "tsv", sep='.')
+# PLOT_ONOFF=paste(paste(OUTPUT, "onoff", sep='/'), ALPHABET, DATASET, DISTANCE, PLENGTH, "pdf", sep='.')
+# 
+# if ((R = load_file(FILENAME_ONOFF))$ok)
+# {
+#   TABLE_ONOFF <- R$tsv;
+# } else
+#   print(paste("NOT FOUND:", FILENAME_ONOFF))
+# 
+# table_onoff = subset(TABLE_ONOFF, alphabet==ALPHABET & dataset==DATASET & plength==PLENGTH & errors==ERRORS, select=c(filter, pcount, time))
+# table_onoff <- transform(table_onoff, time = time / pcount)
+# 
+# ggplot() +
+#   geom_line(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), linetype='solid') +
+#   geom_point(data=table_onoff, aes(x=pcount, y=time, group=filter, shape=filter, color=filter), size=POINT_SIZE) +
+#   scale_shape_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+#   scale_color_discrete(name="Filter", breaks=FILTER_NAMES, labels=FILTER_LABELS) +
+#   xlab("Patterns") +
+#   ylab("Time (seconds/patterns)") +
+#   scale_x_log10() +
+#   theme_bw(base_size=FONT_SIZE, base_family=FONT_FAMILY)
+# 
+# ggsave(file=PLOT_ONOFF, scale=SCALE, device=cairo_pdf) 
 
 
 ### PLOT VERIFICATIONS ###

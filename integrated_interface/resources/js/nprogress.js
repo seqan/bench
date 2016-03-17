7101bb434f0269b41cc8a568e6d3040dddcf75ce
xp3i4 <cxpan1988@gmail.com>
2016-03-17 16:19:07 +0100

    updates

--- integrated_interface/resources/js/nprogress.js
+++ integrated_interface/resources/js/nprogress.js
@@ -17,16 +17,17 @@
   NProgress.version = '0.2.0';
 
   var Settings = NProgress.settings = {
-    minimum: 0.08,
-    easing: 'linear',
+    minimum: 0.01,
+    easing: 'ease',
     positionUsing: '',
-    speed: 350,
-    trickle: true,
-    trickleSpeed: 250,
-    showSpinner: true,
+    speed: 200,
+    trickle: false,
+    trickleRate: 0.02,
+    trickleSpeed: 800,
+    showSpinner: false,
     barSelector: '[role="bar"]',
     spinnerSelector: '[role="spinner"]',
-    parent: 'body',
+    parent: '#progressbar',
     template: '<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
   };
 
@@ -82,16 +83,16 @@
 
       if (n === 1) {
         // Fade out
-        css(progress, {
-          transition: 'none',
-          opacity: 1
+        css(progress, { 
+          transition: 'none', 
+          opacity: 1 
         });
         progress.offsetWidth; /* Repaint */
 
         setTimeout(function() {
-          css(progress, {
-            transition: 'all ' + speed + 'ms linear',
-            opacity: 0
+          css(progress, { 
+            transition: 'all ' + speed + 'ms linear', 
+            opacity: 0 
           });
           setTimeout(function() {
             NProgress.remove();
@@ -160,26 +161,9 @@
 
     if (!n) {
       return NProgress.start();
-    } else if(n > 1) {
-      return;
     } else {
       if (typeof amount !== 'number') {
-        if (n >= 0 && n < 0.25) {
-          // Start out between 3 - 6% increments
-          amount = (Math.random() * (5 - 3 + 1) + 3) / 100;
-        } else if (n >= 0.25 && n < 0.65) {
-          // increment between 0 - 3%
-          amount = (Math.random() * 3) / 100;
-        } else if (n >= 0.65 && n < 0.9) {
-          // increment between 0 - 2%
-          amount = (Math.random() * 2) / 100;
-        } else if (n >= 0.9 && n < 0.99) {
-          // finally, increment it .5 %
-          amount = 0.005;
-        } else {
-          // after 99%, don't increment:
-          amount = 0;
-        }
+        amount = (1 - n) * clamp(Math.random() * n, 0.1, 0.95);
       }
 
       n = clamp(n + amount, 0, 0.994);
@@ -188,7 +172,7 @@
   };
 
   NProgress.trickle = function() {
-    return NProgress.inc();
+    return NProgress.inc(Math.random() * Settings.trickleRate);
   };
 
   /**
@@ -236,7 +220,7 @@
     if (NProgress.isRendered()) return document.getElementById('nprogress');
 
     addClass(document.documentElement, 'nprogress-busy');
-
+    
     var progress = document.createElement('div');
     progress.id = 'nprogress';
     progress.innerHTML = Settings.template;
@@ -245,7 +229,7 @@
         perc     = fromStart ? '-100' : toBarPerc(NProgress.status || 0),
         parent   = document.querySelector(Settings.parent),
         spinner;
-
+    
     css(bar, {
       transition: 'all 0 linear',
       transform: 'translate3d(' + perc + '%,0,0)'
@@ -356,7 +340,7 @@
 
   var queue = (function() {
     var pending = [];
-
+    
     function next() {
       var fn = pending.shift();
       if (fn) {
@@ -371,10 +355,10 @@
   })();
 
   /**
-   * (Internal) Applies css properties to an element, similar to the jQuery
+   * (Internal) Applies css properties to an element, similar to the jQuery 
    * css method.
    *
-   * While this helper does assist with vendor prefixed property names, it
+   * While this helper does assist with vendor prefixed property names, it 
    * does not perform any manipulation of values prior to setting styles.
    */
 
@@ -415,7 +399,7 @@
 
     return function(element, properties) {
       var args = arguments,
-          prop,
+          prop, 
           value;
 
       if (args.length == 2) {
@@ -446,7 +430,7 @@
     var oldList = classList(element),
         newList = oldList + name;
 
-    if (hasClass(oldList, name)) return;
+    if (hasClass(oldList, name)) return; 
 
     // Trim the opening space.
     element.className = newList.substring(1);
@@ -470,13 +454,13 @@
   }
 
   /**
-   * (Internal) Gets a space separated list of the class names on the element.
-   * The list is wrapped with a single space on each end to facilitate finding
+   * (Internal) Gets a space separated list of the class names on the element. 
+   * The list is wrapped with a single space on each end to facilitate finding 
    * matches within the list.
    */
 
   function classList(element) {
-    return (' ' + (element && element.className || '') + ' ').replace(/\s+/gi, ' ');
+    return (' ' + (element.className || '') + ' ').replace(/\s+/gi, ' ');
   }
 
   /**
@@ -489,3 +473,4 @@
 
   return NProgress;
 });
+

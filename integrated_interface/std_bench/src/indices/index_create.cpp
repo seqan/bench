45528bf2f092b232dc4bc530d3d4da97c7a9a3c0
marehr <marehr@zedat.fu-berlin.de>
2016-04-29 11:49:31 +0200

    Create all needed indices

--- integrated_interface/std_bench/src/indices/index_create.cpp
+++ integrated_interface/std_bench/src/indices/index_create.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include <seqan/seq_io.h>
 #include <seqan/arg_parse.h>
 #include <seqan/index.h>
@@ -7,26 +8,26 @@
 using namespace seqan;
 
 struct Options {
-    CharString inputFile;
-    CharString outputFile;
+    std::string inputFile;
+    std::string outputFile;
 };
 
 seqan::ArgumentParser::ParseResult parseCommandLine(Options & options, int argc, char** const argv)
 {
     seqan::ArgumentParser parser("Global Pairwise Alignment benchmark demo");
-    setCategory(parser, "Demo");
+    setCategory(parser, "Benchmark");
     setVersion(parser, "0.1");
-    setDate(parser, "Nov 2015");
+    setDate(parser, "Apr 2016");
 
     addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fIIN\\fP \\fIOUT\\fP ");
 
     addDescription(parser,
-    "\\fIIN\\fP is a .fa / fasta input file.  \\fIOUT\\fP is a inx output file.");
+    "\\fIIN\\fP is a .fa / fasta input file.  \\fIOUT\\fP is a collections of created indices.");
 
     addArgument(parser, ArgParseArgument(ArgParseArgument::INPUT_FILE, "IN"));
     addArgument(parser, ArgParseArgument(ArgParseArgument::OUTPUT_FILE, "OUT"));
     setValidValues(parser, 0, "FASTS, fa");
-    setValidValues(parser, 1, "inx");
+    // setValidValues(parser, 1, "inx");
 
     addSection(parser, "Settings");
 
@@ -37,6 +38,18 @@ seqan::ArgumentParser::ParseResult parseCommandLine(Options & options, int argc,
         getArgumentValue(options.outputFile, parser, 1);
     }
     return res;
+}
+
+template <typename TIndexSpec, typename TString>
+void create_index(TString & genome, std::string filename)
+{
+    Index<TString, TIndexSpec > index(genome);
+    Finder<Index<TString, TIndexSpec > > finder(index);
+
+    TString needle = "TTTTATGGTAACGATACATTAAGGATAGTTTTTTAGAGTTCACGGTTGAA";
+    find(finder, needle);
+
+    save(index, toCString(filename));
 }
 
 int main(int argc, char **argv)
@@ -51,15 +64,21 @@ int main(int argc, char **argv)
     SeqFileIn seqFileIn(toCString(options.inputFile));
     readRecord(idGenome, genome, seqFileIn);
 
-    Index<String<Dna5>, IndexEsa<> > index(genome), openIndex;
-    Finder<Index<String<Dna5>, IndexEsa<> > > esaFinder(index);
+    // create sa index
+    create_index<IndexSa<> >(genome, options.outputFile + "_sa.inx");
 
-    indexRequire(index, EsaSA());
-    indexRequire(index, EsaLcp());
-    indexRequire(index, EsaChildtab());  // for TopDown iterators
-    indexRequire(index, EsaBwt());       // for (Super-)MaxRepeats iterators
+    // create esa index
+    create_index<IndexEsa<> >(genome, options.outputFile + "_esa.inx");
 
-    save(index, toCString(options.outputFile));
-    open(openIndex, toCString(options.outputFile));
-    std::cout << length(openIndex) << std::endl;
+    // create fm index
+    create_index<FMIndex<> >(genome, options.outputFile + "_fm.inx");
+
+    // create wotd index
+    create_index<IndexWotd<> >(genome, options.outputFile + "_wotd.inx");
+
+    // create dfi index
+    create_index<IndexWotd<Dfi<> > >(genome, options.outputFile + "_wotd_dfi.inx");
+
+    // create qgram index
+    create_index<IndexQGram<UngappedShape<50>, OpenAddressing > >(genome, options.outputFile + "_qgram.inx");
 }

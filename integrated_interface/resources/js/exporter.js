00245ec96adbe1ef49060aafe8cdce67ce049b39
marehr <marehr@zedat.fu-berlin.de>
2016-05-18 15:51:35 +0200

    Start exporter

---
+++ integrated_interface/resources/js/exporter.js
@@ -0,0 +1,56 @@
+;(function(root, factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Exporter = factory();
+    }
+
+})(this, function() {
+    var self = {};
+
+    self.generate_results = function(benchmark_queue) {
+        // TODO benchmark_queue.started_at
+        var benchmark_results = {
+            informations: {
+                system: Configure.system_infos(),
+                project: {
+                    title: 'seqan-2.1-gcc'
+                }
+            },
+            results: {
+            }
+        };
+
+        // prepare benchmark_results.results
+        for(const benchmark of benchmark_queue) {
+            const id = benchmark.benchmark_id;
+            benchmark_results.results[id] = {
+                "single_core": {
+                    "time": []
+                },
+                "multi_core": {
+                    "time": []
+                }
+            };
+        }
+
+        // fill results
+        for(const benchmark of benchmark_queue) {
+            const id = benchmark.benchmark_id;
+            var result = benchmark_results.results[id];
+
+            if (benchmark.threads == 1) {
+                result.single_core.time.push(benchmark.runtime);
+            } else {
+                result.multi_core.time.push(benchmark.runtime);
+            }
+        }
+
+        return benchmark_results;
+    };
+
+    return self;
+});

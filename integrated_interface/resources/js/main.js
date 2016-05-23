0f495547c64283d86a3943e424d13720a1eee56c
marehr <marehr@zedat.fu-berlin.de>
2016-05-23 16:57:53 +0200

    WIP: remove main.js

--- integrated_interface/resources/js/main.js
+++
@@ -1,7 +0,0 @@
-nw.Window.open('./resources/index.html', {}, function(new_win) {
-    new_win.window.document.addEventListener("DOMContentLoaded", function(event) {
-        const renderer = require('jsrender');
-        var tmpl = renderer.templates('./resources/templates/gui/body.html');
-        new_win.window.document.body.innerHTML = tmpl.render();
-    });
-});
